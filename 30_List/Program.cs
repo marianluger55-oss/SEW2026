f7a977cc7518104cbb726bcb7211898404e6eef9
marianluger55-oss <marianluger55-oss>
2026-01-16 10:57:19 +0100

    fix

--- 30_List/Program.cs
+++ 30_List/Program.cs
@@ -3,3 +3,10 @@ List<int> numbers = new List<int>(); //keine Größenangabe
 
 numbers.Add(10);
 numbers.Add(20);
+
+int nrofElements = numbers.Count; //readonly Property
+Console.WriteLine(nrofElements);
+
+Console.WriteLine(numbers[1]); //indizierter Zugriff 
+
+numbers[1] = 5; //indizierter Zugriff
